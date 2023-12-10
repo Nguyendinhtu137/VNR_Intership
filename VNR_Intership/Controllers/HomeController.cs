c9889ee8abe31db89bdd5850ca804afd65b595ff
Admin_Tu <Admin_Tu@DESKTOP-5LU39CL>
2023-12-11 02:44:38 +0700

    Add project files.

---
+++ VNR_Intership/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VNR_Intership.EFData;
+using VNR_Intership.Models;
+
+namespace VNR_Intership.Controllers
+{
+    public class HomeController : Controller
+    {
+        VNR_InternShipEntities db = new VNR_InternShipEntities();
+        public ActionResult Index(int? id)
+        {
+            var response = new KhoaHocModel();
+            response.KhoaHocs = db.KhoaHocs.ToList();
+            response.MonHocs = db.MonHocs.Where(x => x.KhoaHocID == id).ToList();
+            var khoahoc = db.KhoaHocs.Where(x => x.ID == id).FirstOrDefault();
+            if (khoahoc != null)
+            {
+                response.Tenkhoahoc = "Khóa học: " + khoahoc.TenKhoaHoc;
+            }
+            return View(response);
+        }
+    }
+}
---
+++ VNR_Intership/Models/KhoaHocModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VNR_Intership.EFData;
+
+namespace VNR_Intership.Models
+{
+    public class KhoaHocModel
+    {
+        public string Tenkhoahoc { get; set; }
+        public List<KhoaHoc> KhoaHocs { get; set; }
+        public List<MonHoc> MonHocs { get; set; }
+    }
+}
